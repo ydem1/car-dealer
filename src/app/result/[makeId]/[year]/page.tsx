9127970d8e045cc1e75855a68a973f3f6f018577
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2025-02-01 18:10:17 +0200

    fix

--- src/app/result/[makeId]/[year]/page.tsx
+++ src/app/result/[makeId]/[year]/page.tsx
@@ -5,32 +5,32 @@ import { IResponseData } from "@/@types/api";
 import { IMake } from "@/@types/make";
 
 export async function generateStaticParams() {
-  const { data } = await instance.get<IResponseData<IMake[]>>(
-    "/GetMakesForVehicleType/car?format=json"
-  );
+  try {
+    const { data } = await instance.get<IResponseData<IMake[]>>(
+      "/GetMakesForVehicleTye/car?format=json"
+    );
+    const makes = data.Results.map((make) => make.MakeId);
+    const years = generateYearOptions().map((year) => year.value);
 
-  const makes = data.Results.map((make) => make.MakeId);
-  const years = generateYearOptions().map((year) => year.value);
+    const paths = [];
 
-  const paths = [];
-
-  for (const make of makes) {
-    for (const year of years) {
-      paths.push({ makeId: make.toString(), year: year.toString() });
+    for (const make of makes) {
+      for (const year of years) {
+        paths.push({ makeId: make.toString(), year: year.toString() });
+      }
     }
-  }
 
-  return paths.map((params) => ({
-    params,
-  }));
+    return paths.map((params) => ({
+      params,
+    }));
+  } catch (error) {
+    console.error("Error fetching makes data:", error);
+    return [];
+  }
 }
 
-const Result = async ({
-  params,
-}: {
-  params: { makeId: string; year: string };
-}) => {
-  const { makeId, year } = await params;
+const Result = ({ params }: { params: { makeId: string; year: string } }) => {
+  const { makeId, year } = params;
 
   return (
     <section className="container py-20">
